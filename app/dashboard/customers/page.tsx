6e34a1d5dbd0717d71ea2471ff9913232f986d93
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2023-10-28 22:24:26 +0330

    before config data base

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Cuto</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Invoise</div>
+  )
+}
+
+export default Page
